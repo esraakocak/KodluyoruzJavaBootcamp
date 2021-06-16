5cf346a24afebb06905983970ff048d59af8b5c6
esraakocak <esra1kocak@gmail.com>
2021-06-16 22:23:40 +0300

    Asal Sayi

---
+++ Homework26/src/Homework26.java
@@ -0,0 +1,23 @@
+
+public class Homework26 {
+
+	public static void main(String[] args) {
+		
+		int sayac=0;
+		for(int sayi=2 ; sayi <=100 ; sayi++) {
+			
+			int kontrol =0;
+			for(int i=2; i<sayi; i++) {
+				if(sayi % i==0) {
+					kontrol=1;
+					break;
+				}
+			} 
+			if(kontrol == 0) {
+				System.out.println(sayi+"\n");
+				sayac++;
+			}
+		}
+	}
+
+}
