f54ee39bf82a73d69b24130cc5f23900238d26b4
esraakocak <esra1kocak@gmail.com>
2021-06-19 09:05:56 +0300

    Recursive

---
+++ Recursive_Asal/src/Asal.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Asal {
+
+	
+    static boolean isAsal(int n, int i) {
+
+        if (n <= 2)
+            return (n == 2) ? true : false;
+        if (n % i == 0)
+            return false;
+        if (i * i > n)
+            return true;
+      
+        // Check for next divisor
+        return isAsal(n, i + 1);
+    }
+	public static void main(String[] args) {
+		  Scanner inp= new Scanner(System.in);
+	         System.out.print("Sayiyi Giriniz : "); 
+	         int a = inp.nextInt();
+	         if(isAsal(a, 2))
+	             System.out.println("Sayi asal");
+	         else
+	             System.out.println("Sayi asal deðil");
+	    }
+
+	}
+
+
