025e588c71d4a9334eef09ea970475827d61664a
esraakocak <esra1kocak@gmail.com>
2021-06-20 16:16:29 +0300

    boks maçı

---
+++ Homework32/src/Main.java
@@ -0,0 +1,12 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	    Fighter marc = new Fighter("march" , 15 , 100, 90, 0);
+	      Fighter alex = new Fighter("alex" , 10 , 95, 100, 0);
+	      Ring r = new Ring(marc,alex , 90 , 100);
+	      r.run();
+	}
+
+}
