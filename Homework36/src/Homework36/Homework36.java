6e85aa256c55fab20afae81641dc39d822ce5e53
esraakocak <esra1kocak@gmail.com>
2021-06-23 16:05:36 +0300

    Dizideki Tekrar Eden Sayıları Bulan Program

---
+++ Homework36/src/Homework36/Homework36.java
@@ -0,0 +1,42 @@
+package Homework36;
+
+public class Homework36 {
+
+	
+		
+		static boolean isFind(int[] arr, int value) {
+	        for (int i : arr) {
+	            if (i == value) {
+	                return true;
+	            }
+	        }
+
+	        return false;
+	    }
+
+	    public static void main(String[] args) {
+	        int[] list = {3, 7, 3, 3, 2, 9, 10, 21, 1, 33, 9, 1};
+	        int[] duplicate = new int[list.length];
+	        int startIndex = 0;
+	        for (int i = 0; i < list.length; i++) {
+	            for (int j = 0; j < list.length; j++) {
+	                if ((i != j) && (list[i] == list[j])) {
+	                    if (!isFind(duplicate, list[i])) {
+	                        duplicate[startIndex++] = list[i];
+	                    }
+	                    break;
+	                }
+	            }
+	        }
+
+	        for (int value : duplicate){
+	            if (value != 0){
+	                System.out.println(value);
+	            }
+	        }
+	    }
+		
+		
+	
+
+}
